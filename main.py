87b61460b7962b6654646e70c4f86937badb9278
PH.CH.CITY <cityphch@gmail.com>
2025-05-21 11:08:03 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+"""
+This project upgrade form project old, 
+it' name project KILLER. By My old name APT2600
+"""
+import os,sys
+import time
+
+key = []
+keys = 'install update upgrade set'
+keyh = 'tool hack Tool TOOL'
+help_c = '\033[31m Error code, Dont have this code. pleas \033[36m help \033[31mfor support.'
+##################
+def main():
+    os.system('clear')
+    os.system('toilet XDos-001')
+    print ("     Ahutor.      : PH.CH.CITY ")
+    print ("     Codename.    : XDos-001 ")
+    print ("     Version.     : 1.0v ")
+    print ("     Github.      : https://github.com/Askmechalot01")
+    key.append(input('Wich you want? -> '))
+##################
+def function():
+  for loop in key:
+    if 'help' in key:
+       print ('help')
+    elif 'update' in key:
+         os.system('figlet LOADING...')
+#      os.system('chmod +x *')
+#       os.system('./setup.sh')
+#        main()
+    elif 'tool' in key:
+         os.system('figlet WEB TOOL')
+  #       os.system('chmod +x *')
+ #        os.system('xtool')
+    elif 'code' in key:
+         os.system('chmod +x *')
+         #os.system('xcode')
+#    elif ''
+    else:
+        os.system('clear')
+        os.system('toilet XDos-001')
+        print ("     Ahutor.      : PH.CH.CITY ")
+        print ("     Codename.    : XDos-001 ")
+        print ("     Version.     : 1.0v ")
+        print ("     Github.      : https://github.com/Askmechalot01")
+        print (help_c)
+        key.append(input('Wich you want? -> '))
+main()
+function()
+# PROJECTx
+# PROJECTx
